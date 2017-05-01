8f8b62098fe690475bc156cf0989619f9b0028c5
yujiangmin <yujiangmin@gmail.com>
2017-05-01 09:29:33 -0500

    made application more general

--- ComplexProject/src/your/first/project/MainClass.java
+++ ComplexProject/src/your/first/project/MainClass.java
@@ -3,7 +3,7 @@ package your.first.project;
 public class MainClass {
 
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello universe");
 	}
 
 }
